708f41355dd1692445b913e50d8a24e9fddcd19b
Dane Everitt <dane@daneeveritt.com>
2018-07-26 23:39:40 -0700

    Fix mobile navigation

--- .vuepress/theme/SidebarButton.vue
+++ .vuepress/theme/SidebarButton.vue
@@ -5,25 +5,3 @@
     </svg>
   </div>
 </template>
-
-<!--<style lang="stylus">
-@import './styles/config.styl'
-
-.sidebar-button-x
-  display none
-  width 1.25rem
-  height 1.25rem
-  position absolute
-  padding 0.6rem
-  top 0.6rem
-  left 1rem
-  .icon
-    display block
-    width 1.25rem
-    height 1.25rem
-
-@media (max-width: $MQMobile)
-  .sidebar-button
-    display block
-</style>
--->
