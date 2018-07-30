2035eb2e1a7434ccc6d617b88d2e97236e8d7fc8
Jakob Schrettenbrunner <dev@schrej.net>
2018-07-30 18:19:46 +0200

    add favicons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,6 +10,16 @@ module.exports = {
             },
         },
     },
+    head: [
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: 'image/png', href: '/favicons/favicon-32x32.png', sizes: '32x32' }],
+        ['link', { rel: 'icon', type: 'image/png', href: '/favicons/favicon-16x16.png', sizes: '16x16' }],
+        ['link', { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#0e4688' }],
+        ['link', { rel: 'manifest', href: '/favicons/manifest.json' }],
+        ['link', { rel: 'shortcut icon', href: '/favicons/favicon.ico' }],
+        ['meta', { name: 'msapplication-config', content: '/favicons/browserconfig.xml' }],
+        ['meta', { name: 'theme-color', content: '#0e4688' }],
+    ],
     themeConfig: {
         repo: 'pterodactyl/panel',
         docsRepo: 'pterodactyl/documentation',
