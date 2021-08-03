e9d113452dfffb26a29c2c9bf5eeeadbc8c2248f
Charles Morgan <sir3lit@gmail.com>
2021-08-03 17:46:12 -0400

    docs: bump panel version in the sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -145,7 +145,7 @@ module.exports = {
                     currentVersion: '1.0',
                     versions: [
                         {
-                            title: '1.4',
+                            title: '1.5',
                             name: '1.0',
                             status: 'stable',
                             children: [
